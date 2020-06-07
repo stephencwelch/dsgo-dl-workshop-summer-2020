997ee5beb7f48950410a90d34c695d2650af411f
stephencwelch <stephencwelch@gmail.com>
2020-06-07 18:10:43 -0400

    data getters

---
+++ utils.py
@@ -0,0 +1,6 @@
+import sys
+
+def progress_bar(current, total, width=80):
+    progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
+    sys.stdout.write("\r" + progress_message)
+    sys.stdout.flush()
