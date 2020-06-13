5d94bf79bb9217560b7e7fb775214a7d49fae210
swelchm <stephencwelch@gmail.com>
2020-06-13 16:34:49 -0400

    3 notebooks sp in place, still need to test

--- util/get_and_unpack.py
+++ util/get_and_unpack.py
@@ -1,11 +1,16 @@
-import wget, argparse, zipfile, os
+import wget, argparse, zipfile, tarfile, os, sys
 from pathlib import Path
+
+def simple_progress_bar(current, total, width=80):
+    progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
+    sys.stdout.write("\r" + progress_message)
+    sys.stdout.flush()
 
 def get_and_unpack(url, location='data/'):
 	print('\033[1m' + ' Downloading...this might take a little while...' + '\033[0m')
 
 	Path(location).mkdir(exist_ok=True)
-	filename = wget.download(url, out=location)
+	filename = wget.download(url, out=location, bar=simple_progress_bar)
 
 	print('\n' +  '\033[1m' + ' Unzipping...' + '\033[0m')
 	zip_ref = zipfile.ZipFile(filename, 'r')
@@ -19,6 +24,7 @@ if __name__ == "__main__":
 	parser.add_argument("-url", dest='url', required=True, help='web_address')
 	args = parser.parse_args()
 	get_and_unpack(args.url)
+
 
 
 
